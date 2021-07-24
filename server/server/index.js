13d62684fac1996aff7a90839959286f9fc074ce
Theracon <54535899+Theracon@users.noreply.github.com>
2021-07-24 17:10:35 +0100

    Finalize deploy to heroku

--- server/server/index.js
+++ server/server/index.js
@@ -34,6 +34,10 @@ io.on("connection", (socket) => {
 
 app.use(express.static(path.resolve(__dirname, "../../client/build")));
 
+app.get("/api", (req, res) => {
+  res.json({ message: "EMR server." });
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
 });
